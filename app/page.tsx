9444584d7e1d149a01e51305a065aa3a69e1a649
bala00790 <ssbalaji2004@gmail.com>
2025-05-25 15:13:51 +0530

    implement homepage and companionform

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
-import {Button} from "@/components/ui/button";
-import {text} from "node:stream/consumers";
-
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
+import {recentSessions} from "@/constants";
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">hellow there!</h1>
-        <Button className="">lets get started</Button>
-    </div>
+    <main>
+      <h1 >Popular Companions</h1>
+
+        <section className="home-section">
+            <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="Science"
+            duration={45}
+            color="#ffda6e"/>
+
+            <CompanionCard
+            id="123"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="Maths"
+            duration={30}
+            color="#e5d0ff"/>
+
+            <CompanionCard
+            id="123"
+            name="Verba the Vocabulary Builder"
+            topic="English Literature "
+            subject="English"
+            duration={30}
+            color="#BDE7FF"/>
+
+        </section>
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+                <Cta />
+        </section>
+    </main>
   )
 }
 
