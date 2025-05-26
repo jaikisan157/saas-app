35acab669564c502f625511a20f9c4f0b1757d0d
bala00790 <132342219+bala00790@users.noreply.github.com>
2025-05-26 18:17:48 +0530

    Implemented vapi voice assistant

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import {currentUser} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {getSubjectColor} from "@/lib/utils";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps{
     params: Promise<{id:string}>;
@@ -10,9 +11,10 @@ interface CompanionSessionPageProps{
 
 const CompanionSession = async  ({params}: CompanionSessionPageProps) => {
     const {id} = await params;
-    const { name, subject, title ,topic, duration} = await getCompanion(id);
+    const companion = await getCompanion(id);
     const user = await currentUser();
 
+    const { name, subject, title ,topic, duration} = companion;
     if(!user) redirect('/sign-in');
     if(!name) redirect('/companions');
     return (
@@ -46,6 +48,12 @@ const CompanionSession = async  ({params}: CompanionSessionPageProps) => {
                     {duration} minutes
                 </div>
             </article>
+            <CompanionComponent
+                {...companion}
+                companionId={id}
+                userName={user.firstName!}
+                userImage={user.imageUrl!}
+            />
         </main>
     );
 };
